ad910f41451573d3af0bf7e46182b936b1642937
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-06-18 14:57:47 +0300

    сделал темы

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import menu from './menu.json'
+
+//  Шаблон Разметки
+
+console.log(menu);
+
+
+
+//  Меняем тему
+
+const checkBoxTheme = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+if (localStorage.theme === 'true') {
+    body.classList.add(Theme.LIGHT, Theme.DARK);
+    checkBoxTheme.checked = true
+
+} else {
+    localStorage.setItem('theme', false);
+    body.classList.add(Theme.LIGHT)
+}
+
+checkBoxTheme.addEventListener('change', changeTheme);
+
+function changeTheme(ev) {
+localStorage.setItem('theme', ev.target.checked)
+
+body.classList.toggle(Theme.DARK);
+};
+
+
+
